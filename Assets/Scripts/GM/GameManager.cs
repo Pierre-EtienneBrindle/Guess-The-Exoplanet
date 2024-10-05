3306e15dafbc27f54cc750e38487fc5f67b542f5
AlexendreGosselin <Alexendre.Gosselin@usherbrooke.ca>
2024-10-05 14:26:55 -0400

    GM V1.1 ---   Tout va etre plus facile.

--- Assets/Scripts/GM/GameManager.cs
+++ Assets/Scripts/GM/GameManager.cs
@@ -10,35 +10,40 @@ public class GameManager : SingletonMonobehavior<GameManager>
     }
 
 
+    public void ChangeScene(PossibleScenes scene)
+    {
+        switch (scene)
+        {
+            case PossibleScenes.Menu:
+                SLoad("title scene");
+                break;
+            case PossibleScenes.MiniG1:
+                SLoad("PlyaerMovementTest");
+                break;
+            case PossibleScenes.MiniG2:
+                SLoad("");
+                break;
+            case PossibleScenes.MiniG3:
+                SLoad("");
+                break;
+            case PossibleScenes.MiniG4:
+                SLoad("");
+                break;
+        }
+    }
+
+
     protected override void Awake()
     {
         base.Awake();
     }
+}
 
 
-    public void MenuLoad()
-    {
-        
-        SLoad("test1");
-    }
-    public void PlayerMoverLoad()
-    {
-        SLoad("PlyaerMovementTest");
-    }
-    public void WikiLoad()
-    {
-        SLoad("Wiki");
-    }
-    public void StarCounterLoad()
-    {
-        SLoad("StarCounterMG");
-    }
-    public void MiniGame2Load()
-    {
-        SLoad("");
-    }
-    public void MiniGame3Load()
-    {
-        SLoad("");
-    }
+public enum PossibleScenes {
+    Menu,
+    MiniG1,
+    MiniG2,
+    MiniG3,
+    MiniG4
 }
