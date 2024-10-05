2aada7b980f2abee0c4a6fe246aa4d94f7f0473c
AlexendreGosselin <Alexendre.Gosselin@usherbrooke.ca>
2024-10-05 15:25:55 -0400

    GM commenter wow!

--- Assets/Scripts/GM/GameManager.cs
+++ Assets/Scripts/GM/GameManager.cs
@@ -4,18 +4,25 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : SingletonMonobehavior<GameManager>
 {
+    //because I cannot TAB SceneManager.LoadScene()
     private void SLoad(string scene_name)
     {
         SceneManager.LoadScene(scene_name);
     }
 
-
+    //The Big fonction that change scene, be sure to write the correct scene string name
     public void ChangeScene(PossibleScenes scene)
     {
         switch (scene)
         {
             case PossibleScenes.Menu:
                 SLoad("title scene");
+                break;
+            case PossibleScenes.Credit:
+                SLoad("Credit");
+                break;
+            case PossibleScenes.Ship:
+                SLoad("Ship");
                 break;
             case PossibleScenes.MiniG1:
                 SLoad("PlyaerMovementTest");
@@ -39,9 +46,11 @@ public class GameManager : SingletonMonobehavior<GameManager>
     }
 }
 
-
+// All possible reference for button to change the game state
 public enum PossibleScenes {
     Menu,
+    Credit,
+    Vaisseau,
     MiniG1,
     MiniG2,
     MiniG3,
