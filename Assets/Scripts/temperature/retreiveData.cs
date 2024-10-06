0551ce0a11c7d1d653267310fc9bd4f3a17396a4
charlo18 <boudreaultca@gmail.com>
2024-10-06 18:09:44 -0400

    minijeupresquefini

--- Assets/Scripts/temperature/retreiveData.cs
+++ Assets/Scripts/temperature/retreiveData.cs
@@ -1,19 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class retreiveData : MonoBehaviour
 {
     float temperature;
+    [SerializeField] private Button b_1;
+    [SerializeField] private Button b_2;
+    [SerializeField] private Button b_3;
+
+    private bool b_b_1;
+    private bool b_b_2;
+    private bool b_b_3;
+
+    [SerializeField] private GameObject sphere1;
+    [SerializeField] private GameObject sphere2;
+    [SerializeField] private GameObject sphere3;
+
+    [SerializeField] private GameObject tempDisplay;
     // Start is called before the first frame update
     void Start()
     {
-        
+        b_b_1 = false;
+        b_b_2 = false;
+        b_b_3 = false;
+        b_1.onClick.AddListener(() => { b_b_1 = true;  });
+        b_2.onClick.AddListener(() => { b_b_2 = true; });
+        b_3.onClick.AddListener(() => { b_b_3 = true; Debug.Log("test"); });
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(b_b_1)
+        {
+            b_1.enabled = false;
+            sphere1.SetActive(false);
+        }
+        if(b_b_2)
+        {
+            b_2.enabled = false;
+            sphere2.SetActive(false);
+        }
+        if(b_b_3)
+        {
+            b_3.enabled = false;
+            sphere3.SetActive(false);
+        }
+        if(b_b_1 && b_b_2 && b_b_3)
+        {
+            //mission reussi
+            temperature = 130;
+            tempDisplay.GetComponent<TMP_Text>().SetText("the temperature is " + temperature + " K");
+            tempDisplay.SetActive(true);
+        }
     }
 }
