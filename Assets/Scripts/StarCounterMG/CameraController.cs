f66020476c8bf3e75a4cf9962ecf956362e10754
Pierre-EtienneBrindle <pebrindle1@gmail.com>
2024-10-06 07:37:49 -0400

    Fixed issues with the MG

--- Assets/Scripts/StarCounterMG/CameraController.cs
+++ Assets/Scripts/StarCounterMG/CameraController.cs
@@ -2,43 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class CameraController : MonoBehaviour
 {
-    public float speed = 1f;
+    [SerializeField] float speed = 1f;
     
+    [SerializeField] Vector2 minBounds;
+    [SerializeField] Vector2 maxBounds;
 
-    public Vector2 minBounds;
-    public Vector2 maxBounds; 
-    
-   
+    PlayerInput.SpaceShipActions actions;
+    float width; 
+    float height;
+    private void Awake()
+    {
+        Camera cam = GetComponent<Camera>();
+        height = 2 * cam.orthographicSize;
+        width = height * cam.aspect;
+    }
+
+    private void Start()
+    {
+        actions = InputManager.Instance.GetSpaceShipActions();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 position = transform.position; 
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            position.x -=  speed * Time.deltaTime ;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            position.x +=  speed * Time.deltaTime ;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            position.y -= speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            position.y += speed * Time.deltaTime;
-        }
-        
+        Vector3 position = transform.position;
+        Vector2 input = speed* Time.deltaTime * actions.Movement.ReadValue<Vector2>();
+        position.y += input.y;
+        position.x += input.x;
 
         //clamp the new position to the defined borders 
-        position.x = Mathf.Clamp(position.x, minBounds.x, maxBounds.x);
-        position.y = Mathf.Clamp(position.y, minBounds.y, maxBounds.y);
-        
+        position.x = Mathf.Clamp(position.x, minBounds.x + width, maxBounds.x - width);
+        position.y = Mathf.Clamp(position.y, minBounds.y + height, maxBounds.y - height);
 
         transform.position = position;
     }
