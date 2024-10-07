959ba2927421172b0ed33c6f52feb0fb94d267ef
Pierre-EtienneBrindle <pebrindle1@gmail.com>
2024-10-06 22:42:20 -0400

    COMMIT

--- Assets/Scripts/temperature/retreiveData.cs
+++ Assets/Scripts/temperature/retreiveData.cs
@@ -5,17 +5,6 @@ using UnityEngine;
 public class retreiveData : MonoBehaviour
 {
     float temperature;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void SetTemperature(float value) //function called when the game is started 
         => temperature = value;
